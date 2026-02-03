69cd43f95bbae31366aabd20e6f18244104e10a0
GabrielOS019 <gabrielosantos019@gmail.com>
2026-02-03 10:28:10 -0300

    Correção na aula 3

--- src/aula3/aula3.java
+++ src/aula3/aula3.java
@@ -7,15 +7,25 @@ public class aula3 {
     static void main(){
         Scanner sc = new Scanner(System.in);
 
-        IO.println("Digite a sua nota");
-        int nota = sc.nextInt();
+        IO.println("Digite as suas notas");
+
+        IO.println("\nNota 1:");
+        int n1 = sc.nextInt();
+
+        IO.println("\nNota 2:");
+        int n2 = sc.nextInt();
+
+        IO.println("\nNota 3:");
+        int n3 = sc.nextInt();
+
+        int nota = (n1+n2+n3)/3;
 
         if (nota <= 4){
-            IO.println("Você foi reprovado devio a sua média ter sido menor que 4 \nNota total:" + nota);
+            IO.println("\nVocê foi reprovado devio a sua média ter sido menor que 4 \nMédia total:" + nota);
         } else if (nota <= 6) {
-            IO.println("Você está de recuperação, pois a sua nota é maior que 4, todavia menor que 7 \nNota total:" + nota);
+            IO.println("\nVocê está de recuperação, pois a sua nota é maior que 4, todavia menor que 7 \nMédia total:" + nota);
         }else if (nota <= 10){
-            IO.println("Você está aprovado! Sua nota é maior que 7! \nNota total:" + nota);
+            IO.println("\nVocê está aprovado! Sua nota é maior que 7! \nMédia total:" + nota);
         }
     }
 }
